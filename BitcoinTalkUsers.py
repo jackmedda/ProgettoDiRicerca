1ca821d6a44a182177b2e9c77539ff93fe191cee
Giacomo <jackm.medda@gmail.com>
2019-03-15 13:44:33 +0100

    First setup project for scraping Bitcoin Talk users profiles

---
+++ BitcoinTalkUsers.py
@@ -0,0 +1,11 @@
+from bs4 import BeautifulSoup
+
+def main():
+    filepath = "BitcoinTalkScrapingResult.txt"
+    soup = BeautifulSoup("")
+    with open(filepath, "w") as txt:
+
+
+
+if __name__ == "__main__":
+    main()
