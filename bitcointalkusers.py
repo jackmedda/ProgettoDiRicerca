b0b7bca31264c2c37b27c1791f9187f1adf6a04f
Giacomo <jackm.medda@gmail.com>
2019-03-19 16:36:54 +0100

    Removed bitcoin validator. Chose to save data in file and then analyze file. Need to do the part for writing in file.

--- bitcointalkusers.py
+++ bitcointalkusers.py
@@ -1,44 +1,61 @@
 from urllib.request import urlopen, Request
 from lxml import etree
 from bitcoinaddressvalidator import check_bc
+from time import sleep
+
 
 def main():
-    url = "https://bitcointalk.org/index.php?action=profile;u=60001"
-    html = getHTML(url)
+    url = "https://bitcointalk.org/index.php?action=profile;u="
 
-    page = etree.HTML(html)
+    filepath = "BitcoinTalkScrapingResult.txt"
+    with open(filepath, "w") as txt:
+        for u in range(4, 100):#2562718):
+            sleep(1)
+            html = getHTML(url + str(u))
+            page = etree.HTML(html)
 
-    for b in page.getiterator('b'):
-        if b.text in ('Bitcoin address: ', 'Name: ', 'Location: ', 'Signature: '):
-            td = b.getparent().getnext()
+            if not isemptypage(page):
+                txt.write(str(u) + ' ' + getfeatures(page) + '\n')
+
+
+def isemptypage(page):
+    emptyuser = page.find("body//tr[@class='titlebg']/td")
+    return emptyuser is not None and emptyuser.text == "An Error Has Occurred!"
+
+
+def getfeatures(page):
+    result = ''
+    for b in page.iter('b'):
+        if b.text in ('Bitcoin address: ', 'Name: ', 'Location:', 'Signature:'):
+            if b.text == "Signature:":
+                td = b.getparent().getparent().getnext().find('td/div')
+            else:
+                td = b.getparent().getnext()
             if td is not None:
                 if td.text is not None:
-                    print(b.text + td.text)
-    #for table_row in page.findall("body//table[@cellpadding='2']/tr/td"):
-    #    print(table_row.tag)
-    #    print(table_row.text)
+                    #if b.text == 'Bitcoin address: ':
+                        #if check_bc(td.text):
+                            #print(b.text + td.text)
+                    #else:
+                        if b.text in ('Location:', 'Signature:'):
+                            print(b.text + ' ' + td.text)
+                        else:
+                            print(b.text + td.text)
+                else:
+                    print(b.text)
 
-    #print(page.find("/body/div/table/tbody/tr/td/table/tbody/tr[2]/td/table/tbody"))
-    #print (page.findtext("address"))
-
-    #filepath = "BitcoinTalkScrapingResult.txt"
-    #soup = BeautifulSoup("")
-    #with open(filepath, "w") as txt:è
-    #    for u in range(4,2562718):
-    #        print ""
+    return result
 
 
 def getHTML(url):
     req = Request(url=url, headers={
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'})
-    fp = urlopen(req)
+    resource = urlopen(req)
 
-    bytepage = fp.read()
+    content = resource.read().decode(resource.headers.get_content_charset())
+    resource.close()
 
-    htmlpage = bytepage.decode("utf8")
-    fp.close()
-
-    return htmlpage
+    return content
 
 
 if __name__ == "__main__":
