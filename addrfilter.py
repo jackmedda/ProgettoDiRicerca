a931291503fba68857a96c34e305c58a664919ac
Giacomo <jackm.medda@gmail.com>
2019-03-28 11:57:42 +0100

    Addes Ethereum address regex. #DOING: working on other cryptocurrencies address format to create new regexs.

--- addrfilter.py
+++ addrfilter.py
@@ -9,14 +9,14 @@ __all__ = [
 
 _BITCOIN_REGEX = r'(?<=\W)[13][1-9A-HJ-NP-Za-km-z]{25,34}'
 _BITCOIN_BECH32_REGEX = r'(?<=\W)bc1[02-9ac-hj-np-z]{6,87}'
-_ETHEREUM_REGEX = ""
-_BITCOIN_CASH_REGEX = ""
-_LITECOIN_REGEX = ""
-_DOGECOIN_REGEX = ""
-_DASH_REGEX = ""
-_BITCOIN_SV_REGEX = ""
-_BINANCE_COIN_REGEX = ""
-_MAKER_REGEX = ""
+_ETHEREUM_REGEX = r'(?<=\W)0x[0-9a-fA-F]{40}'
+_BITCOIN_CASH_REGEX = r'(?<=\W)'
+_LITECOIN_REGEX = r'(?<=\W)'
+_DOGECOIN_REGEX = r'(?<=\W)'
+_DASH_REGEX = r'(?<=\W)'
+_BITCOIN_SV_REGEX = r'(?<=\W)'
+_BINANCE_COIN_REGEX = r'(?<=\W)'
+_MAKER_REGEX = r'(?<=\W)'
 
 res = [
     ([re.compile(_BITCOIN_REGEX), re.compile(_BITCOIN_BECH32_REGEX)], "Bitcoin address"),
@@ -48,6 +48,7 @@ globals().update(_Address.__members__)
 def findaddressesbytype(s, address_type):
     regex = res[address_type.value]
 
+    print(extractaddress(regex, s))
     return extractaddress(regex, s)
 
 
@@ -55,13 +56,15 @@ def findalladdresses(s):
     useraddrs = set()
     for regex in res:
         useraddrs.update(extractaddress(regex, s))
+
+    print(useraddrs)
     return useraddrs
 
 
 def extractaddress(regex, s):
     useraddrs = set()
     addrs = []
-    print(regex)
+
     if type(regex[0]) == list:
         for reg in regex[0]:
             addrs.extend(reg.findall(s))
@@ -69,7 +72,7 @@ def extractaddress(regex, s):
         addrs = regex[0].findall(s)
 
     for addr in addrs:
-        if addr is not None:
+        if addr:
             useraddrs.add((regex[1], addr))
     return useraddrs
 
@@ -79,7 +82,7 @@ str1 = "Like my answer? Did I help? Tips 19G6VFcV1qZJxe3Swn28xz3F8gDKTznwEM grat
 
 str2 = "amiller on freenode / 19G6VFcV1qZJxe3Swn28xz3F8gDKTznwEM"
 
-str3 = "3862 West Fork Drive Plantation, FL 33324" # NO
+str3 = "3862 West Fork 0x9Ca0e998dF92c5351cEcbBb6Dba82Ac2266f7e0C Drive Plantation, FL 33324" # NO
 
 str4 = "plus size black dresses (http://www.swakdesigns.com/c-134-black-dresses.aspx)" # NO
 
@@ -87,9 +90,11 @@ str5 = "Philips norelco 1280cc combining gyroflex 3d, ultratrack heads and sking
 
 str6 = "3139" # NO
 
-str7 = "plus size black bc1qwqdg6squsna38e46795at95yu9atm8azzmyvckulcc7kytlcckxswvvzej dresses (http://www.swakdesigns.com/c-134-black-dresses.aspx) -1H6wM8Xj8GNrhqWBrnDugd8Vf3nAfZgMnq"
+str7 = "plus size 0xCd2a3d9f938e13Cd947eC05ABC7fe734df8DD826 black bc1qwqdg6squsna38e46795at95yu9atm8azzmyvckulcc7kytlcckxswvvzej dresses (http://www.swakdesigns.com/c-134-black-dresses.aspx) -1H6wM8Xj8GNrhqWBrnDugd8Vf3nAfZgMnq"
 
 strings = (str1, str2, str3, str4, str5, str6, str7)
 
 findalladdresses(str7)
-findaddressesbytype(str7, _Address.ETHEREUM)
+findaddressesbytype(str7, _Address.BITCOIN)
+
+findaddressesbytype(str3, _Address.ETHEREUM)
